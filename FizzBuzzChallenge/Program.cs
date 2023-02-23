535bbdba9863c87596c50cf6fc7e063acb991759
Amy Galbicsek <acgalbicsek@gmail.com>
2023-02-23 14:07:26 -0900

    FizzBuzzChallenge Complete

---
+++ FizzBuzzChallenge/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Reflection;
+
+
+namespace Whiteboard 
+{
+    public static class FizzBuzz
+    {
+        public static string FizzOrBuzz(int number)
+        {
+            var result = "";
+            if (number % 3 == 0)
+            {
+                result = "fizz";
+            }
+            if (number % 5 == 0)
+            {
+                result += "buzz";
+            }
+            return result;
+            //var num = 0;
+            //if(num % 3 == 0)
+            //{
+            //    return "fizz";
+
+            //}
+            //else if(num % 5 == 0)
+            //{
+            //    return "buzz";
+            //}
+            //if(num % 3 == 0 && num % 5 == 0)
+            //{
+            //    return "FizzBuzz";
+            //}
+        }
+
+    }
+    
+}
+
+
+ 
